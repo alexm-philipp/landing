f16912732ab409965cb3b8eba3ac9347112cc7bf
Alex Philipp <alexm.philipp@icloud.com>
2025-09-20 00:15:57 +0400

    Add CSP headers to allow GTM scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com;"
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
