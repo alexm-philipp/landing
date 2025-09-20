0126e203f89729bb02d67871ada5a74b00126a79
Alex Philipp <alexm.philipp@icloud.com>
2025-09-20 22:52:14 +0400

    perfromance web updates

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,9 @@ export default function Head() {
       <>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://www.googletagmanager.com" />
-        <link rel="preconnect" href="https://www.google-analytics.com" />
+        <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://www.google-analytics.com" crossOrigin="anonymous" />
       </>
     );
   }
+  
--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;
+              script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com;
               connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://*.google-analytics.com;
               img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
